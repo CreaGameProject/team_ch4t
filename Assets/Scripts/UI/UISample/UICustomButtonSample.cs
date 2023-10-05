067f0471efbc4265f04180e29a70d631408c3d93
KeichiMizutani <m.keichi16256@gmail.com>
2023-10-05 11:12:23 +0900

    [add] 長押しボタンのサンプル作成

---
+++ Assets/Scripts/UI/UISample/UICustomButtonSample.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace UI.UISample
+{
+    public class UICustomButtonSample : MonoBehaviour
+    {
+        [SerializeField] private CustomButton _button;
+        private void Start()
+        {
+            _button.OnClickAction = () =>
+            {
+                Debug.Log("Click");
+            };
+            _button.OnLongPressAction = () =>
+            {
+                Debug.Log("LongPress");
+            };
+        }
+    }
+}
