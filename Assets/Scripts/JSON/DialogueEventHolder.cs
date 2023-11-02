1353615dc22a178305a7d93d542ac6dcfd1a77d4
KeichiMizutani <m.keichi16256@gmail.com>
2023-11-02 15:24:31 +0900

    [add] JSONの読み込み

--- Assets/Scripts/JSON/DialogueEventHolder.cs
+++ Assets/Scripts/JSON/DialogueEventHolder.cs
@@ -1,29 +1,4 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class DialogueEventHolder : MonoBehaviour
+public class DialogueEventHolder
 {
-    private TextAsset jsonAsset;
-    [SerializeField] List<DialogueEvent> dialogueEventList;
-
-    private void Start()
-    {
-        jsonAsset = Resources.Load<TextAsset>("JSON/text_event_test");
-        
-        if (jsonAsset != null)
-        {
-            // JSONデータをデシリアライズしてリストに格納
-            dialogueEventList = JsonUtility.FromJson<List<DialogueEvent>>(jsonAsset.text);
-            foreach (var dialogueEvent in dialogueEventList)
-            {
-                Debug.Log(dialogueEvent.eventNumber);
-            }
-        }
-        else
-        {
-            Debug.LogError("取得できませんでした");
-        }
-    }
+    public DialogueEvent[] dialogueEvents;
 }
