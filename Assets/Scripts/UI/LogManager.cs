c8ec94b7b80d1d58e8a7dc90cbda406fa9c06be8
ShibataniMuku <60276111.shibatani.cgp@gmail.com>
2023-11-11 00:21:13 +0900

    [add]ログウィンドウを作成
    ログウィンドウを作成した。

---
+++ Assets/Scripts/UI/LogManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class LogManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject nodeGroup;
+    [SerializeField]
+    private GameObject yourLogNode;
+    [SerializeField]
+    private GameObject partnerLogNode;
+    [SerializeField]
+    private GameObject scrollView;
+    private ScrollRect scrollRect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollRect = scrollView.GetComponent<ScrollRect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AddPartnerLog("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddYourLog("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        }
+    }
+
+    /// <summary>
+    /// 会話ログにあなたの発言を追加
+    /// </summary>
+    /// <param name="log">あなたの発言</param>
+    public void AddYourLog(string log)
+    {
+        GameObject sb =  Instantiate(yourLogNode, nodeGroup.transform);
+        sb.GetComponent<LogNode>().SetContent(log);
+        StartCoroutine(AnimateScrollView());
+    }
+
+    /// <summary>
+    /// 会話ログに相手の発言を追加
+    /// </summary>
+    /// <param name="log">相手の発言</param>
+    public void AddPartnerLog(string log)
+    {
+        GameObject sb =  Instantiate(partnerLogNode, nodeGroup.transform);
+        sb.GetComponent<LogNode>().SetContent(log);
+        StartCoroutine(AnimateScrollView());
+    }
+
+    private IEnumerator AnimateScrollView()
+    {
+        yield return new WaitForSeconds(0.1f);
+        nodeGroup.GetComponent<VerticalLayoutGroup>().spacing -= 0.0001f;
+        scrollRect.DOVerticalNormalizedPos(0, 2);
+    }
+}
