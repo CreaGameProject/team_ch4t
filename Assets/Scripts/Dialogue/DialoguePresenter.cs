25210f003a06a1c8950db0f86535a37108ed5b91
KeichiMizutani <m.keichi16256@gmail.com>
2023-11-26 21:50:13 +0900

    [refactor] MVPの調整

--- Assets/Scripts/Dialogue/DialoguePresenter.cs
+++ Assets/Scripts/Dialogue/DialoguePresenter.cs
@@ -4,20 +4,21 @@ using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = System.Random;
 
 public class DialoguePresenter : MonoBehaviour
 {
     [SerializeField] private DialogueModel _model;
     [SerializeField] private DialogueView _view;
-    [SerializeField] private DialogueJsonReader _dialogueJsonReader;
     
     private async void Start()
     {
         //await _view.StartBattleDialogue("テストキャラクラー","Sprites/CAS_character_portraits_for_dialogs_vol1portrait_kohaku_02","テキストてきすと<color=#9c3444>文章</color>text");
         //await _view.StartBattleDialogue("テストキャラクター","Sprites/CAS_character_portraits_for_dialogs_vol1/portrait_kohaku_03","テキストてきすと<color=#9c3444>文章</color>text");
         //Debug.Log("話終わりました。");
-        await StartRandomBattleDialogue(_dialogueJsonReader.DialogueTalkEvents, 0);
+        await StartRandomBattleDialogue(_model.DialogueTalkEvents, 0);
+        await StartRandomBattleDialogue(_model.DialogueTalkEvents, 1);
     }
 
     public async UniTask StartRandomBattleDialogue(List<DialogueTalkEvent> dialogueTalkEvents ,int secretCount)
