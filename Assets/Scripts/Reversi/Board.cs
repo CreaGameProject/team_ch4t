d3a4d945bfdeb3f26c73017e83d4b7cf7ccb82a7
TakamasaNishigami <takamasa.nishigami@gmail.com>
2023-11-26 18:06:36 +0900

    Revert "[add] 機能追加"
    This reverts commit 63b9f5bf3f175d6cc066cb33fe89d091c6fc632e.

--- Assets/Scripts/Reversi/Board.cs
+++ Assets/Scripts/Reversi/Board.cs
@@ -44,7 +44,6 @@ public class Board : MonoBehaviour
 
     [Header("コンピュータ")]
     [SerializeField] private Computer computer = null;
-    public Computer.Opponent getOpponent { get { return this.computer.getOpponent; } }
 
     [Header("石のプレハブ")]
     [SerializeField] private GameObject normalStone = null;
@@ -65,15 +64,12 @@ public class Board : MonoBehaviour
     [Header("手数（０になるとゲームオーバー）")] public int restTurn;
     public int getPresetRestTurn { get { return this.restTurn; } } // todo : getRestTurn に変更する
 
-    [Header("プリセットインデックス")]
-    [SerializeField] private int presetIndex = 0;
-    public int getHowManyHimituDidGet { get { return this.presetIndex; } }
-
-
     [Header("盤面と手数のプリセット")]
     [SerializeField] private List<Preset> presets = new List<Preset>();
 
-    
+    //bool didFlipSecretCell = false;
+    /*[Header("プリセットインデックス")]
+    [SerializeField]*/ private int presetIndex = 0;
 
 
 
