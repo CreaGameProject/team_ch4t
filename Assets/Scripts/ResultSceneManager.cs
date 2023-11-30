ca06b602e97d95714c44d476020ab40c083dd849
KeichiMizutani <32954048+KeichiMizutani@users.noreply.github.com>
2023-12-01 06:29:35 +0900

    Feature/result check (#141)
    * [fix] パーティクルのエラー修正
    
    * WebGLテスト

---
+++ Assets/Scripts/ResultSceneManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class ResultSceneManager : MonoBehaviour
+{
+    private void Start()
+    {
+        AudioManager.instance_AudioManager.PlayBGM(1);
+    }
+}
