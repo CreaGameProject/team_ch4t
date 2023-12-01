60ded50699896b42221001803e479a180543c3ca
KeichiMizutani <m.keichi16256@gmail.com>
2023-12-01 23:37:20 +0900

    [fix] カメラエラーまわりの修正

---
+++ Assets/Scripts/ResultScenePresenter.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class ResultScenePresenter : MonoBehaviour
+{
+    [SerializeField] private OutGameDialoguePresenter _presenter;
+    [SerializeField] private SceneChanger _sceneChanger;
+    
+    private CancellationTokenSource cts;
+    
+    private async void Start()
+    {
+        AudioManager.instance_AudioManager.PlayBGM(1);
+        
+        cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        await _presenter.PlayDialogue(1, token);
+        //cts.Cancel();
+        
+        //_sceneChanger.LoadScene("ReversiBasic");
+    }
+    
+    void OnDestroy()
+    {
+        cts?.Cancel();
+        cts?.Dispose();
+    }
+}
