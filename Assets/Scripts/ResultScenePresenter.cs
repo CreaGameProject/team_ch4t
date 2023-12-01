7945ed481405137ce8d347fe3ad86f1d61992e00
KeichiMizutani <32954048+KeichiMizutani@users.noreply.github.com>
2023-12-02 00:52:04 +0900

    Feature/result check (#147)
    * [fix] パーティクルのエラー修正
    
    * WebGLテスト
    
    * [add] タイトル→ダイアログ→バトル　への繋ぎこみ
    
    * [fix] カメラエラーまわりの修正
    
    * [add] 選択肢はできた
    
    * [fix] Json差し替え

--- Assets/Scripts/ResultScenePresenter.cs
+++ Assets/Scripts/ResultScenePresenter.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 public class ResultScenePresenter : MonoBehaviour
 {
     [SerializeField] private OutGameDialoguePresenter _presenter;
+    [SerializeField] private ResultSceneView _view;
     [SerializeField] private SceneChanger _sceneChanger;
     
+    
     private CancellationTokenSource cts;
+    private bool isReported = false;
     
     private async void Start()
     {
+        _view.OnClicked += OnReportButtonClicked;
+        
         AudioManager.instance_AudioManager.PlayBGM(1);
         
         cts = new CancellationTokenSource();
         var token = cts.Token;
 
         await _presenter.PlayDialogue(1, token);
-        //cts.Cancel();
+        await _view.ShowResult();
+
+        if (isReported)
+        {
+            await _presenter.PlayDialogue(2, token);
+        }
+        else
+        {
+            await _presenter.PlayDialogue(3, token);
+        }
+
+        await _presenter.PlayDialogue(4, token);
         
-        //_sceneChanger.LoadScene("ReversiBasic");
+        cts.Cancel();
+        _sceneChanger.LoadScene("Title");
     }
     
     void OnDestroy()
     {
         cts?.Cancel();
         cts?.Dispose();
+    }
+
+    void OnReportButtonClicked(bool isReport)
+    {
+        isReported = isReport;
     }
 }
