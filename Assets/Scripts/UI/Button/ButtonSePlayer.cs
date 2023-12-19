3f016efab76510e590122c9bae5f9ce057023cc7
ShibataniMuku <60276111.shibatani.cgp@gmail.com>
2023-12-19 14:37:39 +0900

    [add]ウィンドウの実装方法を改善
    ・非表示にしてゲームを再生できるように修正
    ・ボタンのイベントをスクリプトから追加するように修正

--- Assets/Scripts/UI/Button/ButtonSePlayer.cs
+++ Assets/Scripts/UI/Button/ButtonSePlayer.cs
@@ -19,7 +19,6 @@ public class ButtonSePlayer : MonoBehaviour
     public void PlayPressedButtonSe()
     {
         AudioManager.instance_AudioManager.PlaySE(0);
-        Debug.Log("aaaaaaa");
     }
 
     public void PlayHighlightedButtonSe()
