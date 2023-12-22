d097c88a80f6b10ee704e0f6d1fc5f4a4c8612c8
ShibataniMuku <60276111.shibatani.cgp@gmail.com>
2023-12-22 10:17:47 +0900

    [fix]ウィンドウの修正
    ・正しく表示されるように、座標を一部調整

---
+++ Assets/Scripts/UI/Kalte/KalteProfile.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KalteProfile : MonoBehaviour
+{
+    [SerializeField]
+    private Image kalteImage;
+    private string birthday;
+    private string gradeAndClass;
+    private int height;
+    private int bodyWeight;
+    private string hobby;
+    private string specialSkill;
+    private string[] secret = new string[3];
+}
